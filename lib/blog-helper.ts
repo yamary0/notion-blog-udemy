8ad3e51284e0d2d4d2ef4ab8ebe3f9d87c19ee03
yamary0 <40788982+yamary0@users.noreply.github.com>
2023-04-17 01:05:42 +0900

    fix typo

--- lib/blog-helper.ts
+++ lib/blog-helper.ts
@@ -1,3 +1,3 @@
 export const getPageLink = (tag: string, page: number) => {
-    return tag ? `/posts/tag/${tag}/page/${page}` : `/posts/pagge/${page}`
+    return tag ? `/posts/tag/${tag}/page/${page}` : `/posts/page/${page}`
 }
