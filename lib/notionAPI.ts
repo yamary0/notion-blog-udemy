0fa172e178378fa06acdbcada5c08070fb735eae
yamary0 <40788982+yamary0@users.noreply.github.com>
2023-04-16 23:33:20 +0900

    tsconfig comment out target

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -12,7 +12,7 @@ const n2m = new NotionToMarkdown({notionClient: notion});
 
 export const getAllPosts = async () => {
 	const posts = await notion.databases.query({
-        database_id: process.env.NOTION_DATABASE_ID as string,
+        database_id: process.env.NOTION_DATABASE_ID,
         page_size: 100,
         filter: {
             property: "Published",
